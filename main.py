b57f5e78bdb7da95d96071412fc355c43d0d125d
Elizaveta <Elizaveta-5512@mail.ru>
2023-09-01 16:40:40 +0300

    add main1

--- main.py
+++ main.py
@@ -5,12 +5,25 @@ from pprint import pprint
 
 code_type = "utf-8"
 url = "recipes.txt"
-cook_book = {}
+cook_book = {
+  'Омлет': [
+    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
+    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
+    ],
+  'Утка по-пекински': [
+    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
+    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
+    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
+    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
+    ],
+  'Запеченный картофель': [
+    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
+    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
+    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
+    ]}
 
 
-
-
-# Функция формирования словаря из текстового файла
 def create_cookbook(url, code_type):
     cook_book = {}
     ingredients_dict = {}
@@ -41,14 +54,3 @@ def create_cookbook(url, code_type):
             cook_book[dish_name] = (dish_ingredients_list.copy())
 
         return cook_book
-
-
-def get_shop_list_by_dishes(dishes, person_count, cook_book):
-    for dish in dishes:
-        for ingredient in cook_book.get(dish):
-            shop_list_by_dishes[ingredient.get('ingredient_name')] = {'measure': ingredient.get('measure'), 'quantity': ingredient.get('quantity') * person_count }
-    return shop_list_by_dishes
-
-
-
-pprint(get_shop_list_by_dishes(order_list, 2, create_cookbook(url, code_type)))
